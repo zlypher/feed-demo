e0ce49b0becc93265e2beecdf302b75bafa7d50b
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-10-19 18:23:29 +0200

    Prototype article detail page

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,3 +1,25 @@
-export default function Article() {
-  return <div>tbd</div>;
+import { getArticleBySlug } from "@/data/articles";
+
+interface IArticleProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Article({ params }: IArticleProps) {
+  const article = getArticleBySlug(params.slug);
+  if (!article) {
+    // TODO: Show 404
+    return null;
+  }
+
+  return (
+    <main className="p-4 flex-1">
+      <header className="mb-4">
+        <h1 className="mb-2 text-2xl">{article?.headline}</h1>
+        <img src="https://placehold.co/600x400/EEE/31343C" />
+      </header>
+      <p>{article.text}</p>
+    </main>
+  );
 }
