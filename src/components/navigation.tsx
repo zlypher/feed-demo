186a1115d0370f0d78679077506439f2a4604923
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-06 22:10:34 +0100

    Rename and restructure navigation. #4

---
+++ src/components/navigation.tsx
@@ -0,0 +1,28 @@
+import {
+  HomeIcon,
+  LayoutGridIcon,
+  ListCheckIcon,
+  UserIcon,
+} from "lucide-react";
+import { NavItem } from "./nav-item";
+
+export const Navigation = () => {
+  return (
+    <nav className=" h-[64px] bg-white w-full bottom-0 shadow-gray-900 shadow-sm">
+      <ul className="flex h-[64px] flex-row w-full justify-around items-center">
+        <NavItem href="/" label="Zum Feed">
+          <HomeIcon />
+        </NavItem>
+        <NavItem href="/favorites" label="Zu den Favoriten">
+          <ListCheckIcon />
+        </NavItem>
+        <NavItem href="/categories" label="Zu den Kategorien">
+          <LayoutGridIcon />
+        </NavItem>
+        <NavItem href="/profile" label="Zum Profil">
+          <UserIcon />
+        </NavItem>
+      </ul>
+    </nav>
+  );
+};
