fb2994392349e6b09c0a029ee3bd39926e660ac6
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-06 15:13:24 +0100

    Setup og:url via environment variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ export const metadata: Metadata = {
     "This is a simple feed demo project. It include a feed of different articles and detail pages for individual articles.",
   robots: { index: false, follow: false },
   openGraph: {
+    url: process.env.DOMAIN ?? "http://localhost:300",
     title: "Feed Demo",
     description:
       "This is a simple feed demo project. It include a feed of different articles and detail pages for individual articles.",
     type: "website",
     images: "/feed-demo.png",
-    url: "/",
   },
 };
 
