82c32fac8382748a5ec9682dcfcf1f0c5246a3a3
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-06 15:34:22 +0100

    Setup dynamic meta tags for article page. #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { Feed } from "@/components/feed";
 import { getAllArticles } from "@/data/articles";
+import { defaultMetadata } from "@/utils/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
+  ...defaultMetadata,
   title: "Home | Feed Demo",
   description:
     "Find the latest and trending news here. You read it here first.",
   openGraph: {
-    url: process.env.DOMAIN ?? "http://localhost:300",
+    ...defaultMetadata.openGraph,
     title: "Home | Feed Demo",
     description:
       "Find the latest and trending news here. You read it here first.",
-    type: "website",
-    images: "/feed-demo.png",
   },
 };
 
