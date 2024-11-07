25eb4c5052f169e625a2cad4bd0b7554b72c3d85
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-07 19:57:23 +0100

    Add simple error state in case there are errors loading the article list. #10

--- src/components/placeholder/palceholder-headline.tsx
+++ src/components/placeholder/palceholder-headline.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export const PlaceholderHeadline = ({ children }: PropsWithChildren) => {
-  return <h1 className="text-2xl font-bold mb-4">{children}</h1>;
+  return <h1 className="text-xl font-bold mb-4">{children}</h1>;
 };
