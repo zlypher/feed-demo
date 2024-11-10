101a3ef7c355c57cabfa231d0c28d12674afadf0
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-10 15:34:41 +0100

    Fix build warning.

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 
 export const defaultMetadata: Metadata = {
+  metadataBase: new URL(process.env.DOMAIN ?? "http://localhost:300/"),
   title: "Feed Demo",
   description:
     "This is a simple feed demo project. It include a feed of different articles and detail pages for individual articles.",
