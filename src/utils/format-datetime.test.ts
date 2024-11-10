7b7dde6ba82db24e19e9084db64b8b8c3eed8318
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-10 20:33:07 +0100

    Fix format-datetime test.

--- src/utils/format-datetime.test.ts
+++ src/utils/format-datetime.test.ts
@@ -3,6 +3,6 @@ import { formatDatetime } from "./format-datetime";
 
 test("formatDatetime should display full date and time", () => {
   expect(formatDatetime(new Date("2024-08-23T14:33:22"))).toBe(
-    "23. August 2024 um 14:33"
+    "23.08.24, 14:33"
   );
 });
