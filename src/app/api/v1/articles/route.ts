7b899ea2f8da3191e86c6808020248bddf1c2c33
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-11-10 14:22:12 +0100

    Test basic logger setup with pino. #28

--- src/app/api/v1/articles/route.ts
+++ src/app/api/v1/articles/route.ts
@@ -1,7 +1,12 @@
 import { getAllArticles } from "@/data/articles";
+import { logger } from "@/utils/logger";
 import { wait } from "@/utils/wait";
 
 export async function GET() {
+  logger.info(
+    { additional: "info" },
+    "Fetch all articles with delay of 1000ms"
+  );
   const articles = getAllArticles();
 
   // To demonstrate potential loading states on the frontend
